24620462f000c15c326d854729a18c0f223fdfd4
Claudio Santos das Virgens <csantos@MB-Pro-de-Claudio.local>
2020-08-07 07:49:01 -0300

    just add user signin validation and error messaging

--- validator/index.js
+++ validator/index.js
@@ -26,3 +26,47 @@ exports.createPostValidator = (req, res, next) => {
     next();
 
 };
+
+exports.userSignupValidator = (req, res, next) => {
+
+    // name is not null and between 4-10 characters
+    req.check("name","Name is Required").notEmpty();
+    req.check("name","Name must be bettwen 4 and 10 characters").isLength({
+        min:4,
+        max: 10
+    })
+
+    // email is not null, valid and normalized
+    req.check("email","Email must be between 3 to 32 characters").matches(/.+\@.+\..+/)
+    .withMessage("Email must contain @")
+    .isLength({
+        min:4,
+        max: 2000
+    });
+
+    // Check for password
+    req.check("password", "Password is required").notEmpty();
+    req.check("password")
+    .isLength({
+        min:6
+    }).withMessage("Password must contain at lest 6 characters")
+    .matches(/\d/)
+    .withMessage("Password must contain a number");
+
+
+    // check for errors
+
+    const errors = req.validationErrors();
+
+    // if error show the first one as they happen
+
+    if(errors) {
+        const firstError = errors.map((error)=> error.msg)[0]
+        return res.status(400).json({error: firstError})
+    }
+
+    // using next middleware
+    
+    next();
+
+}
