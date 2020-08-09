c332323f01a1b63a7cf10b8ba4ae983289a1d3c3
Claudio Santos das Virgens <csantos@MB-Pro-de-Claudio.local>
2020-08-09 13:03:47 -0300

    implementing Signout endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,26 +4,26 @@ const User = require('../models/user');
 const user = require('../models/user');
 
 exports.signup = async (req, res) => {
-    const userExists = await User.findOne({email: req.body.email})
+    const userExists = await User.findOne({ email: req.body.email })
 
-    if(userExists) 
-    return res.status(403).json({
-        error: "Email is taken !"
-    });
+    if (userExists)
+        return res.status(403).json({
+            error: "Email is taken !"
+        });
 
-    const user  = await new User(req.body);
+    const user = await new User(req.body);
     await user.save();
-    res.status(200).json({message: "signup sucess ! "});
-}; 
+    res.status(200).json({ message: "signup sucess ! " });
+};
 
 exports.signin = (req, res) => {
 
     //find the user based on email 
 
     const { email, password } = req.body;
-    User.findOne({email}, (err,user)=> {
+    User.findOne({ email }, (err, user) => {
         //if err or no user
-        if(err || !user){
+        if (err || !user) {
             return res.status(401).json({
                 error: "User with that email does not exist. Please signin."
             });
@@ -31,26 +31,31 @@ exports.signin = (req, res) => {
         // if user is found lets make sure the email and password match
         // creating authenticate methoed in mode and user here
 
-        if(!user.authenticate(password)){
+        if (!user.authenticate(password)) {
             return res.status(401).json({
                 error: "Email and passowrd do nat match"
             });
         };
 
 
-         // generate a token with a user id and secret 
+        // generate a token with a user id and secret 
 
-    const token = jwt.sign({_id: user._id},process.env.JWT_SECRET);
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
 
-    // persisting the token as 't'in cookie with expiry date
+        // persisting the token as 't'in cookie with expiry date
 
-    res.cookie("t", token, {expire: new Date() + 9999});
+        res.cookie("t", token, { expire: new Date() + 9999 });
 
-    // return response with user and token to frontend client 
+        // return response with user and token to frontend client 
 
-    const {_id, name, email } = user;
-    return res.json({token, user: {_id, email, name}});
+        const { _id, name, email } = user;
+        return res.json({ token, user: { _id, email, name } });
 
     });
 
+}
+
+exports.signout = (req, res) => {
+    res.clearCookie("t");
+    return res.status({ message: "Signout Sucessfully!" });
 }
